dcfb649338d6c28054871adfc5b15bda892605d3
SofiaSFaber <sofia17faber@gmail.com>
2020-07-02 23:55:28 -0300

    Alteração de espaçamentos

--- trabalhobanco.py
+++ trabalhobanco.py
@@ -22,9 +22,10 @@ def ReadMardyCSV():
   pd.set_option('display.max_columns', 500)
   pd.set_option('display.width', 1000)
 
-  mardyCSV = pd.read_csv('DB_by_gene.csv',header = 1)
-  mardyCSV.rename(columns = {'Gene name':'GeneName','Found in':'FoundIn','Gene locus':'GeneLocus','AA mutation':'AAMutation',
-                        'Tandem repeat name':'TandemRepeatName','Tandem repeat sequence':'TandemRepeatSequence'}, inplace=True)
+  mardyCSV = pd.read_csv('DB_by_gene.csv', header = 1)
+  mardyCSV.rename(columns = {'Gene name':'GeneName', 'Found in':'FoundIn', 'Gene locus':'GeneLocus', 'AA mutation':'AAMutation',
+                        'Tandem repeat name':'TandemRepeatName', 'Tandem repeat sequence':'TandemRepeatSequence'}, inplace = True)
+  
   return mardyCSV
 
 
@@ -33,21 +34,21 @@ def GetCSVGenes(mardyCSV):
   for i in range(len(mardyCSV.GeneName)):
     genes.append(mardyCSV.GeneName[i])
   
-  genes=sorted(set(genes))
+  genes = sorted(set(genes))
 
   return genes
 
 
 def GetNCBIData(genes):
   Entrez.email = "sofia17faber@gmail.com"
-  results={}
+  results = {}
   remove = []
   
   for i in range(len(genes)):
-    handle = Entrez.esearch(db="nucleotide", term=genes[i])
+    handle = Entrez.esearch(db = "nucleotide", term = genes[i])
     record = Entrez.read(handle)
     if len(record['IdList'])>2:
-      results[genes[i]]=record["IdList"][0],record['IdList'][1],record['IdList'][2]
+      results[genes[i]] = record["IdList"][0], record['IdList'][1], record['IdList'][2]
     else:
       remove.append(genes[i])
   
@@ -56,11 +57,11 @@ def GetNCBIData(genes):
 
   data = {}
   for i in range(len(genes)):
-      handle = Entrez.efetch(db="nucleotide", id=results[genes[i]][0], retmode="xml")
+      handle = Entrez.efetch(db = "nucleotide", id = results[genes[i]][0], retmode = "xml")
       records1 = Entrez.read(handle)
-      handle = Entrez.efetch(db="nucleotide", id=results[genes[i]][1], retmode="xml")
+      handle = Entrez.efetch(db = "nucleotide", id = results[genes[i]][1], retmode = "xml")
       records2 = Entrez.read(handle)
-      handle = Entrez.efetch(db="nucleotide", id=results[genes[i]][2], retmode="xml")
+      handle = Entrez.efetch(db = "nucleotide", id = results[genes[i]][2], retmode = "xml")
       records3 = Entrez.read(handle)
       if ("GBSeq_references" in records1[0].keys() and "GBSeq_references" in records2[0].keys() and "GBSeq_references" in records3[0].keys() and
       "GBSeq_strandedness" in records1[0].keys() and "GBSeq_strandedness" in records2[0].keys() and "GBSeq_strandedness" in records3[0].keys() and
@@ -72,16 +73,15 @@ def GetNCBIData(genes):
       "GBSeq_definition" in records1[0].keys() and "GBSeq_definition" in records2[0].keys() and "GBSeq_definition" in records3[0].keys() and
       "GBSeq_accession-version" in records1[0].keys() and "GBSeq_accession-version" in records2[0].keys() and "GBSeq_accession-version" in records3[0].keys() and
       "GBSeq_definition" in records1[0].keys() and "GBSeq_definition" in records2[0].keys() and "GBSeq_definition" in records3[0].keys()):
-        data[genes[i]]="Primeiro Resultado:",results[genes[i]][0],records1[0]["GBSeq_locus"],records1[0]["GBSeq_length"],records1[0]["GBSeq_strandedness"],records1[0]["GBSeq_moltype"],records1[0]["GBSeq_topology"],records1[0]["GBSeq_division"],records1[0]["GBSeq_definition"],records1[0]["GBSeq_accession-version"],records1[0]["GBSeq_source"],records1[0]["GBSeq_references"],\
-        "Segundo Resultado:",results[genes[i]][1],records2[0]["GBSeq_locus"],records2[0]["GBSeq_length"],records2[0]["GBSeq_strandedness"],records2[0]["GBSeq_moltype"],records2[0]["GBSeq_topology"],records2[0]["GBSeq_division"],records2[0]["GBSeq_definition"],records2[0]["GBSeq_accession-version"],records2[0]["GBSeq_source"],records2[0]["GBSeq_references"],\
-        "Terceiro Resultado:",results[genes[i]][2],records3[0]["GBSeq_locus"],records3[0]["GBSeq_length"],records3[0]["GBSeq_strandedness"],records3[0]["GBSeq_moltype"],records3[0]["GBSeq_topology"],records3[0]["GBSeq_division"],records3[0]["GBSeq_definition"],records3[0]["GBSeq_accession-version"],records3[0]["GBSeq_source"],records3[0]["GBSeq_references"]
+        data[genes[i]] = "Primeiro Resultado:", results[genes[i]][0], records1[0]["GBSeq_locus"], records1[0]["GBSeq_length"], records1[0]["GBSeq_strandedness"], records1[0]["GBSeq_moltype"], records1[0]["GBSeq_topology"], records1[0]["GBSeq_division"], records1[0]["GBSeq_definition"], records1[0]["GBSeq_accession-version"], records1[0]["GBSeq_source"], records1[0]["GBSeq_references"],\
+        "Segundo Resultado:", results[genes[i]][1], records2[0]["GBSeq_locus"], records2[0]["GBSeq_length"], records2[0]["GBSeq_strandedness"], records2[0]["GBSeq_moltype"], records2[0]["GBSeq_topology"], records2[0]["GBSeq_division"], records2[0]["GBSeq_definition"], records2[0]["GBSeq_accession-version"], records2[0]["GBSeq_source"], records2[0]["GBSeq_references"],\
+        "Terceiro Resultado:", results[genes[i]][2], records3[0]["GBSeq_locus"], records3[0]["GBSeq_length"], records3[0]["GBSeq_strandedness"], records3[0]["GBSeq_moltype"], records3[0]["GBSeq_topology"], records3[0]["GBSeq_division"], records3[0]["GBSeq_definition"], records3[0]["GBSeq_accession-version"], records3[0]["GBSeq_source"], records3[0]["GBSeq_references"]
 
   return data
 
 
 def CreateJSON(data):
-
-  json = js.dumps(data,indent=1, separators=('\r\n', ':'), sort_keys=True)
+  json = js.dumps(data, indent = 1, separators = ('\r\n', ':'), sort_keys = True)
   file = open('integrated_databases.json', 'w')
   file.write(json)
   file.close()
